9f5727d6947412b9be229e141517f9a3f3dd784a
Sergey Shevchik <sergey.shevchik@gmail.com>
2020-03-13 16:24:08 +0300

    version 0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wialon_ips",
-    version="0.0.1",
+    version="0.0.3",
     author="Sergey Shevchik",
     author_email="sergey.shevchik@gmail.com",
     description="Simple Wialon IPs protocol emulator",
